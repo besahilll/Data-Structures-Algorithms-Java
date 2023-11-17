87f0702d3e3c9865ab50dbcec48bf93cce59dadd
Sahil Bathla <99262172+besahilll@users.noreply.github.com>
2023-11-17 14:06:02 +0530

    Create Permutations.java
    This is the code find and print permutations of a given string in time complexity of O(n*n!)

---
+++ Backtracking/Permutations.java
@@ -0,0 +1,21 @@
+//find and print all permutations of a string
+class Permutations{
+    public static void findPermutations(String str, String ans){
+        //base condition
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+
+        //recursion
+        for(int i=0;i<str.length();i++){
+            char curr=str.charAt(i);
+            String NewStr=str.substring(0,i) + str.substring(i+1);
+            findPermutations(NewStr, ans+curr);
+        }
+    }
+    public static void main(String[] args){
+        String str="abc";
+        findPermutations(str,"");
+    }
+}
