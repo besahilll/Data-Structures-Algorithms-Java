fe116f950c2f5ba561d44073255e64aaa51b1375
Sahil Bathla <99262172+besahilll@users.noreply.github.com>
2023-12-18 23:06:24 +0530

    Create Large.java

---
+++ Large.java
@@ -0,0 +1,13 @@
+class large{
+   public static void main(String args[]){
+     int n1=70, n2=102, n3=22;
+     if(n1>n2 && n3>n2){
+        System.out.println("largest number is:"+n1);
+     }
+     else if(n2>n3 && n2>n1){
+        System.out.println("Largest number is:"+n2);
+     else{
+        System.out.println("Largest number is:"+n3);
+     }
+}
+}
