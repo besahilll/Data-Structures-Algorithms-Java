aaefbb585f1830a11a8303bfd238ceb70db4ece9
Sahil Bathla <99262172+besahilll@users.noreply.github.com>
2023-12-28 20:14:48 +0530

    Create reverseString.java
    This is the code to reverse a string using Stack

---
+++ Stack/reverseString.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class reverseString{
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<Character>();
+        int idx=0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder result=new StringBuilder(" ");
+        while(!s.isEmpty()){
+            char curr=s.pop();
+            result.append(curr);
+        }
+        return result.toString(); 
+    }
+    
+    public static void main(String[] args){
+        String str="Sahil";
+        System.out.println(reverseString(str));
+    }
+}
