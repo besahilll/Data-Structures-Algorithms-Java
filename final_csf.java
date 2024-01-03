a22d148f80580df5cb167acba244a1b819d56efa
Sahil Bathla <99262172+besahilll@users.noreply.github.com>
2024-01-03 23:29:18 +0530

    Create final_csf.java

---
+++ final_csf.java
@@ -0,0 +1,14 @@
+final class SOCS{
+	int n_o_s=250;
+	final void registration(){
+		System.out.println("Total no. of students in SOCS:"+n_o_s);
+		}
+		
+class Csf{
+	public static void main(){
+		int n_o_s=180;
+		SOCS S= new SOCS();
+		void registration{
+		System.out.println("The registration for the club is:"+s.r);
+		}
+}
