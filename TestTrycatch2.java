a800dd11ee9bf43452d46cf2c86858b9f00d1f9e
besahilll <500091837@stu.upes.ac.in>
2024-01-05 23:37:11 +0530

    Exception Handling example

---
+++ TestTrycatch2.java
@@ -0,0 +1,12 @@
+public class Testtrycatch2{
+	public static void main(String args[]){
+		try{
+			int data=90;
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+		System.out.println("rest of the code..");
+		}
+}
